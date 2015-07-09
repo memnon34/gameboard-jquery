fd0d2256b477fc987f8918bf1e57492bd4a476f0
Daniel Llinas <danllinas@gmail.com>
2015-07-09 17:06:43 -0400

    Added initial files for Wyncode exercise.

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 var main = function() {
+  $(".more-btn").on('click', function(event){
+    event.preventDefault();
+    $(this).next().toggle();
+  });
+  
+  $(".share").on('click', function(){
+    $(this).next().toggle();
+  });
 
+  $('.bell').on('click', function(){
+    $(this).find('.notification').toggleClass('active');
+  });
 };
 
 $(document).ready(main);
